febdfabb8b9d458ad6f462cb4744ad6589259837
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-09-19 15:00:01 +0000

    Updates

--- main.c
+++ main.c
@@ -5,20 +5,59 @@
 #include <sys/ioctl.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include <net/if.h>
 #include <netinet/ether.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <net/if_arp.h>
 
 #include <netinet/in_systm.h>
 
 #define BUFFSIZE 1518
 
-unsigned char buff1[BUFFSIZE];
+unsigned char buffer[BUFFSIZE];
+
 int sockd;
 int on;
+
+/*GENERAL*/
+int received = 0;
+
+ssize_t max_size = 0; //DONE
+ssize_t min_size = SSIZE_MAX; //DONE
+int bytes_received = 0;
+
+float   avg_size = 0; //DONE
+
+/*DATA LINK LAYER*/
+int arp_rep_count   = 0;
+int arp_req_count   = 0;
+
+/*NETWORK LAYER*/
+int icmp_count = 0;
+int icmp_req_count = 0;
+int icmp_rep_count = 0;
+
+/*TRANSPORT LAYER*/
+int tcp_count = 0;
+int udp_count = 0;
+int tcp_conns = 0;
+unsigned char top_tcp_ports[5];
+unsigned char top_udp_ports[5];
+
+/*APPLICATION LAYER*/
+int http_count = 0;
+int dns_count  = 0;
+unsigned char top_sites[5];
+
+
 struct ifreq ifr;
+struct ether_header* eth_header;
+
+struct iphdr* ip_header;
+struct arphdr* arp_header;
 
 int main(int argc,char *argv[])
 {
@@ -29,7 +68,7 @@ int main(int argc,char *argv[])
 	}
 	if((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
 	{
-		printf("Erro na criacao do socket.\n");
+		printf("Erro na criação do socket.\n");
 		exit(1);
 	}
 
@@ -37,17 +76,36 @@ int main(int argc,char *argv[])
 
 	if(ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
 	{
-		printf("erro no ioctl!\n");
+		printf("Erro no ioctl!\n");
 		exit(1);
 	}
+
+	eth_header = (struct ether_header *) buffer;
+	ip_header  = (struct iphdr*) (buffer + sizeof(struct ether_header));
+	arp_header  = (struct arphdr*) (buffer + sizeof(struct ether_header));
+	/*ip_header  = (struct iphdr*) (buffer + sizeof(struct ether_header));*/
 
 	ioctl(sockd, SIOCGIFFLAGS, &ifr);
 	ifr.ifr_flags |= IFF_PROMISC;
 	ioctl(sockd, SIOCSIFFLAGS, &ifr);
 
 	while (1) {
-		recv(sockd,(char *) &buff1, sizeof(buff1), 0x0);
-		printf("MAC Destino: %x:%x:%x:%x:%x:%x \n", buff1[0],buff1[1],buff1[2],buff1[3],buff1[4],buff1[5]);
-		printf("MAC Origem:  %x:%x:%x:%x:%x:%x \n\n", buff1[6],buff1[7],buff1[8],buff1[9],buff1[10],buff1[11]);
+		ssize_t size = recv(sockd, buffer, BUFFSIZE, 0x0);
+
+		++received;
+		bytes_received += size;
+		avg_size = bytes_received/received;
+
+		if(size > max_size)
+			max_size = size;
+		if(size < min_size)
+			min_size = size;
+		if(eth_header->ether_type == 0x0608)
+			printf("arp %d\n", arp_header->ar_op);
+		if(eth_header->ether_type == 0x0008)
+			printf("ip %d\n", ip_header->protocol);
+		/*printf("arp: %d\n",arp_header->arp_op);*/
+		/*else if(eth_header->ether_type == ETHERTYPE_IP)*/
+			/*printf("IP: %d\n",eth_header->ether_type);*/
 	}
 }
