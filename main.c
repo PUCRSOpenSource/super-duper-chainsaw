2650bc076189639dc5f797edffb7d3a21e143f2a
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-09-21 03:27:18 +0000

    Dunno...

--- main.c
+++ main.c
@@ -1,63 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <byteswap.h>
+#include "main.h"
 
-#include <net/if.h>
-#include <netinet/ether.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
+void setup(char* options[]);
+void receiver(void);
+void arp_handler(void);
+void ip_handler(void);
+void icmp_handler(void);
+void tcp_handler(void);
+void udp_handler(void);
+void payload_handler(unsigned char* data, int size);
+void count_application_layer_protocol(unsigned int port_number);
 
-#include <netinet/in_systm.h>
-
-#define BUFFSIZE 1518
-
-unsigned char buffer[BUFFSIZE];
-
-int sockd;
-int on;
-
-/*GENERAL*/
-int received = 0;
-
-ssize_t max_size = 0; //DONE
-ssize_t min_size = SSIZE_MAX; //DONE
-int bytes_received = 0;
-
-float   avg_size = 0; //DONE
-
-/*DATA LINK LAYER*/
-int arp_rep_count   = 0;
-int arp_req_count   = 0;
-
-/*NETWORK LAYER*/
-int icmp_count = 0;
-int icmp_req_count = 0;
-int icmp_rep_count = 0;
-
-/*TRANSPORT LAYER*/
-int tcp_count = 0;
-int udp_count = 0;
-int tcp_conns = 0;
-unsigned char top_tcp_ports[5];
-unsigned char top_udp_ports[5];
-
-/*APPLICATION LAYER*/
-int http_count = 0;
-int dns_count  = 0;
-unsigned char top_sites[5];
-
-
-struct ifreq ifr;
-struct ether_header* eth_header;
-
-struct iphdr* ip_header;
-struct arphdr* arp_header;
+float calculate_percentage(int counter);
 
 int main(int argc,char *argv[])
 {
@@ -66,13 +19,20 @@ int main(int argc,char *argv[])
 		printf("Use %s <IF_NAME>\n", argv[0]);
 		exit(1);
 	}
+
+	setup(argv);
+	receiver();
+}
+
+void setup(char* options[])
+{
 	if((sockd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
 	{
 		printf("Erro na criação do socket.\n");
 		exit(1);
 	}
 
-	strcpy(ifr.ifr_name, argv[1]);
+	strcpy(ifr.ifr_name, options[1]);
 
 	if(ioctl(sockd, SIOCGIFINDEX, &ifr) < 0)
 	{
@@ -80,19 +40,23 @@ int main(int argc,char *argv[])
 		exit(1);
 	}
 
-	eth_header = (struct ether_header *) buffer;
-	ip_header  = (struct iphdr*) (buffer + sizeof(struct ether_header));
-	arp_header  = (struct arphdr*) (buffer + sizeof(struct ether_header));
-	/*ip_header  = (struct iphdr*) (buffer + sizeof(struct ether_header));*/
+	eth_header  = (struct ether_header*) buffer;
+	ip_header   = (struct iphdr*)   (buffer + sizeof(struct ether_header));
+	arp_header  = (struct arphdr*)  (buffer + sizeof(struct ether_header));
+
 
 	ioctl(sockd, SIOCGIFFLAGS, &ifr);
 	ifr.ifr_flags |= IFF_PROMISC;
 	ioctl(sockd, SIOCSIFFLAGS, &ifr);
+}
 
-	while (1) {
+void receiver(void)
+{
+	while (1)
+	{
 		ssize_t size = recv(sockd, buffer, BUFFSIZE, 0x0);
 
-		++received;
+		received++;
 		bytes_received += size;
 		avg_size = bytes_received/received;
 
@@ -100,20 +64,118 @@ int main(int argc,char *argv[])
 			max_size = size;
 		if(size < min_size)
 			min_size = size;
-		u_int16_t ether_type = eth_header->ether_type;
-		if(ether_type == 0x0608 || ether_type == 0x0806){
-			int op = arp_header->ar_op;
-			if (op == 0x0100 || op == 0x0001)
-				++arp_req_count;
-			
-			else if (op == 0x0200 || op == 0x0002)
-				++arp_rep_count;
-		}
-		if(ether_type == 0x0008 || ether_type == 0x0800)
-			printf("ip %d\n", ip_header->protocol);
-		/*printf("arp: %d\n",arp_header->arp_op);*/
-		/*else if(eth_header->ether_type == ETHERTYPE_IP)*/
-			/*printf("IP: %d\n",eth_header->ether_type);*/
+		u_int16_t ether_type = ntohs(eth_header->ether_type);
+		if(ether_type == 0x0806)
+			arp_handler();
+		if(ether_type == 0x0800)
+			ip_handler();
+	}
+}
+
+void arp_handler()
+{
+	u_int16_t arp_operation = ntohs(arp_header->ar_op);
+	if (arp_operation == 0x0001)
+	{
+		arp_req_count++;
+		arp_req_percent = calculate_percentage(arp_rep_count);
 	}
 
+	else if (arp_operation == 0x0002)
+	{
+		arp_rep_count++;
+		arp_rep_percent = calculate_percentage(arp_rep_count);
+	}
+}
+
+void ip_handler()
+{
+
+	unsigned int ip_protocol = (unsigned int)ip_header->protocol;
+	if (ip_protocol == 0x1)
+		icmp_handler();
+
+	else if (ip_protocol == 0x6)
+		tcp_handler();
+
+	else if (ip_protocol == 0x11)
+		udp_handler();
+}
+
+void icmp_handler()
+{
+	icmp_header = (struct icmphdr*) (buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)));
+	unsigned int icmp_type = (unsigned int) icmp_header->type ;
+
+	if(icmp_type == 0x0)
+	{
+		icmp_rep_count++;
+		icmp_rep_percent = calculate_percentage(icmp_rep_count);
+	}
+
+	else if(icmp_type == 0x8)
+	{
+		icmp_req_count++;
+		icmp_req_percent = calculate_percentage(icmp_req_count);
+	}
+
+	icmp_count++;
+}
+
+void tcp_handler()
+{
+	tcp_header  = (struct tcphdr*)  (buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)));
+	unsigned int tcp_dest_port   = (unsigned int) ntohs(tcp_header->dest);
+	unsigned int ack = (unsigned int) tcp_header->ack;
+	unsigned int syn = (unsigned int) tcp_header->syn;
+	if(ack == 1 && syn == 0)
+		++tcp_conns;
+
+	count_application_layer_protocol(tcp_dest_port);
+
+	tcp_count++;
+	tcp_percent = calculate_percentage(tcp_count);
+
+	/*int payload_size = BUFFSIZE - sizeof(struct ether_header) - (unsigned int)(ip_header->ihl)*4 - (unsigned int)tcp_header->doff*4;*/
+	/*unsigned char* payload_data = (char *) (buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)) + sizeof(struct tcphdr));*/
+	/*payload_handler(payload_data, payload_size);*/
+}
+
+void udp_handler()
+{
+	udp_header  = (struct udphdr*)  (buffer + (sizeof(struct ether_header) + sizeof(struct iphdr)));
+	unsigned int udp_dest_port   = (unsigned int) ntohs(udp_header->dest);
+
+	count_application_layer_protocol(udp_dest_port);
+
+	udp_count++;
+	udp_percent = calculate_percentage(udp_count);
+}
+
+void payload_handler(unsigned char* data, int size)
+{
+	printf("size: %u\n", size);
+	printf("data[0]: %c\n", data[0]);
+}
+void count_application_layer_protocol(unsigned int port_number)
+{
+	if(port_number == 80)
+	{
+		http_count++;
+		http_percent = calculate_percentage(http_count);
+	}
+	else if(port_number == 53)
+	{
+		dns_count++;
+		dns_percent = calculate_percentage(dns_count);
+	}
+	else if(port_number == 443)
+	{
+		https_count++;
+		https_percent = calculate_percentage(http_count);
+	}
+}
+float calculate_percentage(int counter)
+{
+	return (100 * counter) / (float)received;
 }
---
+++ main.h
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <byteswap.h>
+
+#include <net/if.h>
+#include <netinet/ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+#include <linux/icmp.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+
+#include <netinet/in_systm.h>
+
+#define BUFFSIZE 1518
+
+unsigned char buffer[BUFFSIZE];
+
+int sockd;
+int on;
+
+/*GENERAL*/
+int received = 0;
+
+ssize_t max_size = 0; //DONE
+ssize_t min_size = SSIZE_MAX; //DONE
+int bytes_received = 0;
+
+float   avg_size = 0; //DONE
+
+/*DATA LINK LAYER*/
+unsigned int arp_rep_count   = 0; //DONE
+unsigned int arp_req_count   = 0; //DONE
+
+float arp_rep_percent = 0; //DONE
+float arp_req_percent = 0; //DONE
+
+/*NETWORK LAYER*/
+unsigned int icmp_count       = 0; //DONE
+unsigned int icmp_req_count   = 0; //DONE
+unsigned int icmp_rep_count   = 0; //DONE
+
+float icmp_req_percent = 0;
+float icmp_rep_percent = 0;
+
+/*TRANSPORT LAYER*/
+unsigned int tcp_count   = 0; //DONE
+unsigned int udp_count   = 0; //DONE
+unsigned int tcp_conns   = 0; //DONE
+float tcp_percent = 0; //DONE
+float udp_percent = 0; //DONE
+
+unsigned char top_tcp_ports[5];
+unsigned char top_udp_ports[5];
+
+/*APPLICATION LAYER*/
+unsigned int http_count    = 0; //DONE
+unsigned int https_count   = 0; //DONE
+unsigned int dns_count     = 0; //DONE
+
+float http_percent  = 0;
+float https_percent = 0;
+float dns_percent   = 0;
+
+unsigned char top_sites[5];
+
+
+struct ifreq ifr;
+struct ether_header* eth_header;
+
+struct iphdr* ip_header;
+struct arphdr* arp_header;
+struct icmphdr* icmp_header;
+struct tcphdr* tcp_header;
+struct udphdr* udp_header;
